39e6046ea3e8e92eff4565a82eb21a21cfe31ac0
A.B.C. Srl - [Reparto Informatico] <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2023-01-02 11:17:23 +0100

    [UPD]

--- abc_efatturazione/__manifest__.py
+++ abc_efatturazione/__manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://www.abcstrategie.it",
 
     'category': 'Einvoice',
-    'version': '14.0.0.14',
+    'version': '14.0.0.15',
 
     'depends': ['base', 'account', 'l10n_it_fatturapa', 'l10n_it_fatturapa_out', 'l10n_it_fatturapa_in', 'sale', 'web', 'mail'],
 
